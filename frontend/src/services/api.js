c6dd337774e5a9a484c66b862458826ff036fb10
JJHarad <jidnyaharad11.com>
2026-03-31 04:26:32 +0530

    added render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api"
+ baseURL: "https://expense-tracker-m82k.onrender.com/api"
 });
 
-// Attach token automatically
+
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
